7d0b606a31d7ee597da566dcf10a8579a5bc6583
Raj-vishesh <visheshtopper99@gmail.com>
2026-02-01 23:33:12 +0530

    component part

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Card from './components/card.jsx'
+import Navbar from './components/Navbar.jsx'
+
+
+const App = () => {
+  return (
+    <div>
+
+      <div className="nav">
+        Hi I am Navbar 
+      </div>     
+      <div className="card">
+        <h1>Vishesh Chahar </h1>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, fuga.</p>
+      </div>
+
+
+    </div> 
+  )
+}
+
+export default App
---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,10 @@
+function card() {
+
+    return  <div className="card">
+                <h1>Vishesh Chahar </h1>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, fuga.</p>
+            </div>
+
+} 
+
+export default card
